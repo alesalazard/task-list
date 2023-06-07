6b3b21269a34c407f7c2c217a4d19b7833b61072
alexandretta <alexsalazar21@gmail.com>
2023-06-06 20:58:47 -0500

    Se mejoraron los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/App.css';
+import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import Navigation from './components/Navigation';
@@ -12,7 +13,8 @@ function App() {
       <BrowserRouter>
       <Navigation />
         <Routes>
-          <Route path='/' element={<Login />}></Route>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
           <Route path='/register' element={<Register />}></Route>
           <Route path='/dashboard' element={<Dashboard />}></Route>
         </Routes>
